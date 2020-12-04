07f213d59aa563bb26cf8103fa95ba78035154e5
SharkdanDaniel <sharkdan_daniel@hotmail.com>
2020-12-04 11:41:52 -0300

    criado as pastas pages, core e shared. criado os modules core, shared e login (dentro de pages)

---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  readonly apiURL = 'http://hcs.dev4.com.br/api/login/login';
+
+  constructor(private http: HttpClient) {}
+
+  postLogin(login) {
+    return this.http.post(`${this.apiURL}`, login);
+  }
+}
