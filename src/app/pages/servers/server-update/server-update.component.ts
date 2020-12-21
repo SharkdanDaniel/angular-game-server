b67b110bf3daf6cea9f8114b1d8c1dba0de072e3
Daniel <sharkdan_daniel@hotmail.com>
2020-12-21 11:52:04 -0300

    Todos os métodos relacionado ao server estão completos

--- src/app/pages/servers/server-update/server-update.component.ts
+++ src/app/pages/servers/server-update/server-update.component.ts
@@ -14,39 +14,44 @@ export class ServerUpdateComponent implements OnInit {
   false: boolean = false;
   serverId: string;
   server: any;
-  form: FormGroup
+  parcels: any[];
+  // form: FormGroup
 
   constructor(
     private serversService: ServersService,
-    private formBuilder: FormBuilder,
+    // private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      name: [null],
-      shared: [null],
-      hasDisease: [null],
-      initialMoney: [null],
-      initialStatsPoints: [null],
-    });
+    // this.form = this.formBuilder.group({
+    //   name: [null],
+    //   shared: [null],
+    //   hasDisease: [null],
+    //   initialMoney: [null],
+    //   initialStatsPoints: [null],
+    // });
     this.serverId = this.route.snapshot.paramMap.get('id');
     this.serversService.getServerById(this.serverId)
       .pipe(take(1))
       .subscribe((server) => {
+        this.parcels = server.parcels;
         this.server = server;
-        this.form = this.formBuilder.group(server);
-        console.log(this.form);
+        // this.form = this.formBuilder.group(server);
+        console.log(this.server);
       }
     );
   }
 
   update() {
-    console.log(this.server)
-    this.server = this.form.value;
+    const id = '00000000-0000-0000-0000-000000000000';
+    this.parcels.forEach((x) => {
+      x.id = id;
+    });
+    // console.log(this.server)
     this.serversService
-      .updateServer(this.form.value)
+      .updateServer(this.server)
       .pipe(take(1))
       .subscribe((res) => {
         console.log(res);
